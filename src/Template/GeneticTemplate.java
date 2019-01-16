e77dd87519c772e2a622d0a3d01c3c856c9c2f79
Julien Raillard <ju.raillard@hotmail.fr>
2019-01-16 15:30:48 +0100

    HotFix CrossMutate

--- src/Template/GeneticTemplate.java
+++ src/Template/GeneticTemplate.java
@@ -136,15 +136,18 @@ public abstract class GeneticTemplate {
 		List<Individual> newIndividuals = new ArrayList<>();
 		Random rand = new Random();
 		int tempRandValue = 0;
-				
+		Individual tempIndiv = null;
+		
 		for(int i=0; i< nbChildrenToGenerate; i++)
 		{
 			tempRandValue = rand.nextInt(100)+1;
+			tempIndiv = selectedPopulation.get(i).Crossing(selectedPopulation.get(i+1));
+			
 			//Probability to get mutation
 			if(1 >= tempRandValue && tempRandValue <= mutationProbability) 
-				newIndividuals.add(selectedPopulation.get(i).Mutate());	
-			else
-				newIndividuals.add(selectedPopulation.get(i).Crossing(selectedPopulation.get(i+1)));
+				tempIndiv = tempIndiv.Mutate();		
+			
+			newIndividuals.add(tempIndiv);
 		}
 		
 		return newIndividuals;
